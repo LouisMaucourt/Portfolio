f93ee4345421f81cbf2f91c1bb8608648567e39c
Louis-maucourt <louis.maucourt7@gmail.com>
2024-07-23 17:30:05 +0200

    wip

--- src/app/components/sections/BentoPresentation.jsx
+++ src/app/components/sections/BentoPresentation.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import SwitchTheme from '../ui-toggle/SwitchTheme'
-import SwitchTypography from '../ui-toggle/SwitchTypography'
-import Cube from '../sections/Cube'
-import Link from 'next/link'
+import React from 'react';
+import SwitchTheme from '../ui-toggle/SwitchTheme';
+import SwitchTypography from '../ui-toggle/SwitchTypography';
+import Cube from '../sections/Cube';
 
 const BentoPresentation = () => {
     return (
-        <section className=" border-color border-b flex flex-row min-h-[95vh]">
+        <section className="border-color border-b flex flex-row min-h-[95vh]">
             <div className="w-1/6 flex items-center flex-col justify-between min-h-[95vh] border-color border-r">
                 <div className='flex items-center h-[60vh] border-color border-b w-full'>
-                    <Cube></Cube>
+                    <Cube />
                 </div>
-                <div className='flex items-center justify-center h-[40vh] '>
+                <div className='flex items-center justify-center h-[40vh]'>
                     <SwitchTheme />
                 </div>
             </div>
             <div className="w-2/3 flex flex-col justify-between min-h-[95vh] border-color">
                 <div className="h-[50vh] flex items-end justify-center">
-                    <h1 className='text-6xl font-bold uppercase'>Louis Maucourt<br />Développeur Front-End</h1>
+                    <h1 className='text-6xl font-bold uppercase'>
+                        Louis Maucourt<br />
+                        Développeur Front-End
+                    </h1>
                 </div>
-                <div className="h-25vh flex items-center justify-center h-[30vh] border-color border-t" >
-                    <p className="p-9">Après trois ans dans le digital, je suis en mastère de développement web. Passionné par le "creative code", je trouve dans le code le point de convergence de tout mon travail, où l'art et la technologie se rencontrent. Autodidacte, j'explore des animations interactives et des expériences utilisateur innovantes. Cette approche m'a inculqué discipline et curiosité, me permettant d'aborder chaque projet avec l'envie d'apprendre et de créer des solutions uniques.
+                <div className="h-[30vh] flex items-center justify-center border-color border-t">
+                    <p className="p-9">
+                        Après trois ans dans le digital, je suis en mastère de développement web. Passionné par le &quot;creative code&quot;, je trouve dans le code le point de convergence de tout mon travail, où l&apos;art et la technologie se rencontrent. Autodidacte, j&apos;explore des animations interactives et des expériences utilisateur innovantes. Cette approche m&apos;a inculqué discipline et curiosité, me permettant d&apos;aborder chaque projet avec l&apos;envie d&apos;apprendre et de créer des solutions uniques.
                     </p>
                 </div>
             </div>
@@ -28,10 +31,9 @@ const BentoPresentation = () => {
                 <div className="h-[98vh] flex items-center justify-center">
                     <SwitchTypography />
                 </div>
-
             </div>
-        </section >
-    )
-}
+        </section>
+    );
+};
 
-export default BentoPresentation
+export default BentoPresentation;
